170638696110a467cead1d9517da42db9c6421eb
NeoXsa <neoxsa8@gmail.com>
2025-12-20 20:30:22 +0530

    fix: correct useLocation invocation and enhance scroll behavior in ScrollToTop component

--- src/util/ScrollToTop.jsx
+++ src/util/ScrollToTop.jsx
@@ -3,10 +3,10 @@ import { useEffect } from "react";
 
 function ScrollToTop() {
 
-    const { pathname } = useLocation;
+    const { pathname } = useLocation();
 
     useEffect(() => {
-        window.scrollTo(0, 0)
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
     }, [pathname])
 
     return null
